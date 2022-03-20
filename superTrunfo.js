e8a303ab249cb4dbf9f83b3a4f6b8ef4aa0cba40
Conceição Peres <marcos.quintanilha@midway.com.br>
2022-03-20 15:10:17 -0300

    comite inicial

---
+++ superTrunfo.js
@@ -0,0 +1,73 @@
+var carta1 = {
+    nome: "Mestre Dos Magos",
+    atributos: {
+        ataque: 5,
+        defesa: 8,
+        magia: 10
+    }
+}
+
+var carta2 = {
+    nome: "Vingador",
+    atributos: {
+        ataque: 10,
+        defesa: 7,
+        magia: 6
+    }
+}
+
+var carta3 = {
+    nome: "Hank – O Guarda",
+    atributos: {
+        ataque: 9,
+        defesa: 7,
+        magia: 4
+    }
+}
+
+var carta4 = {
+    nome: "Eric – O Cavaleiro",
+    atributos: {
+        ataque: 5,
+        defesa: 10,
+        magia: 4
+    }
+}
+
+var carta5 = {
+    nome: "Diana – A Acrobata",
+    atributos: {
+        ataque: 6,
+        defesa: 8,
+        magia: 4
+    }
+}
+
+var carta6 = {
+    nome: "Sheila – A Ladina",
+    atributos: {
+        ataque: 4,
+        defesa: 5,
+        magia: 8
+    }
+}
+var carta7 = {
+    nome: "Presto – O mago",
+    atributos: {
+        ataque: 5,
+        defesa: 9,
+        magia: 9
+    }
+}
+var carta8 = {
+    nome: "Bobby – O Bárbaro",
+    atributos: {
+        ataque: 9,
+        defesa: 8,
+        magia: 4
+    }
+}
+
+var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8]
+var cartaJogador
+var cartaMaquina
